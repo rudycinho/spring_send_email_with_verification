803feed23ed6ddb2f59fdc2aaede9a87a8b2996c
r.martinez1 <r.martinez@empresasim.com>
2020-01-15 12:07:37 -0400

    Services created

---
+++ src/main/java/com/rudycinho/springmvc/sending/email/services/ConfirmationTokenService.java
@@ -0,0 +1,15 @@
+package com.rudycinho.springmvc.sending.email.services;
+
+import org.springframework.stereotype.Service;
+
+import com.rudycinho.springmvc.sending.email.models.ConfirmationToken;
+import com.rudycinho.springmvc.sending.email.repositories.ConfirmationTokenRepository;
+
+@Service
+public class ConfirmationTokenService {
+	private ConfirmationTokenRepository confirmationTokenRepository;
+	
+	public ConfirmationToken findByConfirmationToken(String confirmationToken) {
+		return confirmationTokenRepository.findByConfirmationToken(confirmationToken);
+	}
+}
---
+++ src/main/java/com/rudycinho/springmvc/sending/email/services/UserService.java
@@ -0,0 +1,17 @@
+package com.rudycinho.springmvc.sending.email.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.rudycinho.springmvc.sending.email.models.User;
+import com.rudycinho.springmvc.sending.email.repositories.UserRepository;
+
+@Service
+public class UserService {
+	@Autowired
+	private UserRepository userRepository;
+	
+	public User findByEmailId(String emailId) {
+		return userRepository.findByEmailId(emailId);
+	}
+}
