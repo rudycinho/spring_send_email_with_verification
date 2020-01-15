16da9822b7bd43a31f082c811a0ed22d392a8eb0
r.martinez1 <r.martinez@empresasim.com>
2020-01-15 11:19:09 -0400

    Models Created

---
+++ src/main/java/com/rudycinho/springmvc/sending/email/models/ConfirmationToken.java
@@ -0,0 +1,43 @@
+package com.rudycinho.springmvc.sending.email.models;
+
+import java.util.Date;
+import java.util.UUID;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Setter
+@Getter
+@Table(name = "confirmation_tokens")
+public class ConfirmationToken {
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private int id;
+	
+	@Column(name = "confirmation_token")
+	private String confirmationToken;
+	
+	@Column(name = "created_date")
+	private Date createdDate;
+	
+	@OneToOne(targetEntity = User.class, fetch = FetchType.EAGER)
+	@JoinColumn(nullable = false, name = "id")
+	private User user;
+	
+	public ConfirmationToken(User user) {
+		this.user = user;
+		createdDate = new Date();
+		confirmationToken = UUID.randomUUID().toString();
+	}
+}
---
+++ src/main/java/com/rudycinho/springmvc/sending/email/models/User.java
@@ -0,0 +1,35 @@
+package com.rudycinho.springmvc.sending.email.models;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@Table(name = "users")
+public class User {
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private int id;
+
+	@Column(name = "mail_id")
+	private String mailId;
+	
+	private String password;
+	
+	@Column(name = "first_name")
+	private String firstName;
+	
+	@Column(name = "last_name")
+	private String lastName;
+	
+	@Column(name = "is_enabled")
+	private boolean isEnabled;
+}
